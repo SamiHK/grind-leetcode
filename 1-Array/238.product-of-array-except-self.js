1862174eb4bbce661b08da4d72012b0a1675b6e4
Sami Haroon <abdulsami.se@gmail.com>
2022-08-20 02:02:10 +0500

    Update 238.product-of-array-except-self.js

--- 1-Array/238.product-of-array-except-self.js
+++ 1-Array/238.product-of-array-except-self.js
@@ -8,9 +8,29 @@
 /**
  * @param {number[]} nums
  * @return {number[]}
+ * @runtime O(n)
+ * @space O(n)
  */
-var productExceptSelf = function(nums) {
+ var productExceptSelf = function(nums) {
     
+    if(nums.length ===0 || nums === null) return [];
+    
+    // filling answers array with Multiplicative identity
+    let [prefix, postfix, LEN] = [1, 1, nums.length]
+    let answers = Array(LEN).fill(1);
+    
+    for(let i = 0; i < LEN; i++){
+        answers[i] = prefix;
+        prefix *= nums[i];
+    }
+    
+    
+    for(let i = LEN-1; i >= 0; i--){
+        answers[i] *= postfix;
+        postfix *= nums[i]
+    }
+    
+    return answers;
 };
 // @lc code=end
 
