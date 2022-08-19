6e2d8573b874483050b105fed77b4d2ead6bb7a0
Sami Haroon <abdulsami.se@gmail.com>
2022-08-19 17:25:57 +0500

    Update 1.two-sum.js

--- 1-Array/1.two-sum.js
+++ 1-Array/1.two-sum.js
@@ -11,7 +11,28 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    // handling edge cases
+    if(target > Infinity || target < -Infinity || nums === null) return -1;
+    if(nums.length < 2) return [];
     
+    let map = new Map();
+    
+    for(let i = 0; i < nums.length; i++){
+        
+        let diff = target - nums[i]; //[7,2,-2,-6]
+        
+        // check if number exist in map
+        if(map.has(String(nums[i]))){
+            // that means a number with difference that makes target exist
+            return [map.get(String(nums[i])), i]
+            
+        } else {
+            // store the difference with index value
+            map.set(String(diff), i);
+        }
+    }
+    
+    return [];
 };
 // @lc code=end
 
