5848dfb0100136391905b097c92eca33a9d0c232
Sami Haroon <abdulsami.se@gmail.com>
2022-08-20 17:48:35 +0500

    Update 125.valid-palindrome.js

--- 2-String/125.valid-palindrome.js
+++ 2-String/125.valid-palindrome.js
@@ -8,9 +8,42 @@
 /**
  * @param {string} s
  * @return {boolean}
+ * @runtime O(n) - string match + half-way execution of all characters
+ * @space O(1)
  */
-var isPalindrome = function(s) {
+ var isPalindrome = function(s) {
     
+    if(s === null || s.length ===0) return -1;
+    
+    s = s.toLowerCase();
+    
+    const regex = /[a-z0-9]/g; // Alphanumeric Regex
+    
+    s = s.match(regex); // only Alphanumeric characters array
+        
+    // <-- shorthand method -->
+    // if you want to save another variant of string save a deep-copy, shallow copy will always match as reverse()
+    // will reverse it as well.
+    // if(s == null || s.join("") === s.reverse().join("")){
+    //     return true;
+    // }
+    
+    if(s=== null || s.length === 1) return true;
+    
+    let [left, right] = [0, s.length-1];
+    while(left <= right) {
+        
+        if(s[left] === s[right]){
+            left++;
+            right--;
+        } else {
+            return false
+        }
+        
+    }
+    
+    // if all characters match till mid-way
+    return true;
 };
 // @lc code=end
 
