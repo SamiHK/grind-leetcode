829ddf3aed2ec2f224c8b05d459f208206cf79bd
Sami Haroon <abdulsami.se@gmail.com>
2022-08-20 21:06:25 +0500

    Longest palindrome substring

--- 2-String/5.longest-palindromic-substring.js
+++ 2-String/5.longest-palindromic-substring.js
@@ -3,46 +3,59 @@
  *
  * [5] Longest Palindromic Substring
  */
-
 // @lc code=start
 /**
  * @param {string} s
- * @return {number}
- * @runtime O(n) - one pass of loop + one pass of map values
- * @space O(n) - map
+ * @return {string}
  */
  var longestPalindrome = function(s) {
-  
-    if(s== null) return -1;
-    if(s.length === 1) return 1;
     
-    // we will store the frequency of the characters, N-evens and 1 odd will be max palindrome
-    const map = new Map();
-    let longestPalindrome = 0;
-    
-    // setting character frequency in case-sensitive keys.
-    for(let i = 0; i < s.length; i++){
-
-        if(map.has(s[i])) {
-            map.set(s[i], map.get(s[i]) + 1)
-        } else {
-            map.set(s[i], 1)
-        }        
+    // edge cases
+    if (s.length === 1) return s;
+    if (s.length === 2) {
+        if (s[0] === s[1]) return s;
+        else return s[0];
     }
-    
-    
-    // checking all map values for even and an odd frequency
-    let haveOdd = 0;
-    map.forEach((value) => { 
-        if(value % 2 === 0) {
-            longestPalindrome += value;
-        } else if(value % 2 !== 0){
-            longestPalindrome += (value-1) // if value is 9 we can use 8 of them in palindrome
-            haveOdd = 1; // always only 1 odd can be in middle of palindrome
-        }
-    });
-    
-    return longestPalindrome + haveOdd;
+
+    let longestPal = '';
+
+    for (let i = 0; i < s.length; i++) {
+        // palindrome can center around 1 or 2 letters
+        let j = i + 1;
+        //abccaaccba
+        // check single character palindrome start
+        const curr1 = findLongestPalindrome(s, i, i);
+        // check same consecutive characters palindrome start
+        const curr2 = findLongestPalindrome(s, i, j);
+
+        const longPal = curr1.length > curr2.length ? curr1 : curr2;
+
+        if (longPal.length > longestPal.length) {
+            longestPal = longPal;
+        } 
+    }
+
+    return longestPal;
 };
+
+const findLongestPalindrome = (str, i, j) => {
+    // abccaaccba (i=4, j=5) this will catch all(aa, caac, ccaacc, bccaaccb, abccaaccba)
+    // abcba (i=2, j=2) this will catch all(c,bcb, abcba)
+    while(i >= 0 && j < str.length && str[i] === str[j]) {
+        i -= 1;
+        j += 1;
+    }
+    // slice the qualified substring from the second last iteration
+    // if match found i+1 gives the correct substring, else we return nothing by i+1,j slicing
+    return str.slice(i + 1, j);
+}
 // @lc code=end
 
+// console.log(longestPalindrome("a"));
+// console.log(longestPalindrome("aa"));
+// console.log(longestPalindrome("ab"));
+
+// console.log("Results: ", longestPalindrome("babad"))
+// console.log(longestPalindrome("cbbd"));
+// console.log(longestPalindrome("abcba"));
+// console.log(longestPalindrome("abccaaccba"));
