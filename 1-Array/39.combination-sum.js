73f58e9a265b46d385a91e480737fd03799612b0
Sami Haroon <abdulsami.se@gmail.com>
2022-08-20 05:32:41 +0500

    Update 39.combination-sum.js

--- 1-Array/39.combination-sum.js
+++ 1-Array/39.combination-sum.js
@@ -9,9 +9,63 @@
  * @param {number[]} candidates
  * @param {number} target
  * @return {number[][]}
+ * @runtime O(2^N)
  */
-var combinationSum = function(candidates, target) {
+ function combinationSum(candidates, target) {
+    var buffer = [];
+    var result = [];
+    search(0, target);
+    return result;
+  
+    function search(startIdx, target) {
+      //buffer is an array which is reference type. 
+      //If we push buffer instead of buffer.slice(), 
+      //the items in the buffer will alter in the following operations.
+
+      // Applying base-case
+      if (target === 0) return result.push(buffer.slice());
+      if (target < 0) return;
+      if (startIdx === candidates.length) return;
+
+      //way forward to add combinations
+      buffer.push(candidates[startIdx]);
+      search(startIdx, target - candidates[startIdx]);
+      //way backward to backtrach and empty memory
+      buffer.pop();
+      search(startIdx + 1, target);
+    }
+  }
+
+// -> PATH SUM LOGIC
+//  var combinationSum = function(candidates, target) {
     
-};
+//     const combs = []
+//     const tempCombs = []
+    
+//     function DFS(index) {     
+        
+//         // base cases
+//         if (target < 0) return
+//         if (target === 0) combs.push([...tempCombs])
+        
+//         // DFS
+//         while (candidates[index]) {
+            
+//             tempCombs.push(candidates[index])
+//             target -= candidates[index]
+            
+//             DFS(index)
+            
+//             target += candidates[index]
+//             tempCombs.pop()
+//             index++
+//         }
+//     }
+    
+//     DFS(0)
+//     return combs;
+// };
+
+// combinationSum([2,3,5], 8) // debugger
 // @lc code=end
 
