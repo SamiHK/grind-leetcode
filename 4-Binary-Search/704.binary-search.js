c8433a8a9438b32db07384b5626e7bdbfefd9373
Sami Haroon <abdulsami.se@gmail.com>
2022-08-21 14:50:12 +0500

    Update 704.binary-search.js

--- 4-Binary-Search/704.binary-search.js
+++ 4-Binary-Search/704.binary-search.js
@@ -9,8 +9,31 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
+ * @runtime O(log n)
+ * @space O(1)
  */
-var search = function(nums, target) {
+ var search = function(nums, target) {
+  
+    if(nums === null || nums.length === 0) return -1
+    
+    let [low, high] = [0, nums.length-1];
+    
+    let mid = low + Math.floor((high-low+1)/2);
+    
+    for(let i = 0; i < nums.length; i++){
+            
+        if(target == nums[mid]){
+            return mid;
+        } else if(target > nums[mid]) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+        
+        mid = low + Math.floor((high-low+1)/2);
+    }
+    
+    return -1;
     
 };
 // @lc code=end
