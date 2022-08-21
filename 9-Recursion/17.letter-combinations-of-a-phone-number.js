2dedda2c845a0d7f0fb89fcf0bfd3abfcceec3d0
Sami Haroon <abdulsami.se@gmail.com>
2022-08-22 04:15:20 +0500

    Update 17.letter-combinations-of-a-phone-number.js

--- 9-Recursion/17.letter-combinations-of-a-phone-number.js
+++ 9-Recursion/17.letter-combinations-of-a-phone-number.js
@@ -9,8 +9,39 @@
  * @param {string} digits
  * @return {string[]}
  */
-var letterCombinations = function(digits) {
-    
-};
+// Time O(3^N * 4^M)
+//   N is the number of digits in the input that maps to 3 letters (e.g. 2, 3, 4, 5, 6, 8)
+//   M is the number of digits in the input that maps to 4 letters (e.g. 7, 9)
+// Space O(3^N * 4^M) since one has to keep O(3^N * 4^M) solutions.
+
+const letterCombinations = (digits) => {
+    if (digits == null || digits.length === 0) return [];
+  
+    const map = {
+      2: 'abc',
+      3: 'def',
+      4: 'ghi',
+      5: 'jkl',
+      6: 'mno',
+      7: 'pqrs',
+      8: 'tuv',
+      9: 'wxyz',
+    };
+  
+    const res = [];
+    const go = (i, s) => {
+      if (i === digits.length) {
+        res.push(s);
+        return;
+      }
+  
+      for (const c of map[digits[i]]) {
+        go(i + 1, s + c);
+      }
+    };
+  
+    go(0, '');
+    return res;
+  };
 // @lc code=end
 
