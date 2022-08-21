7742155350ddb0b03c62aff63a7f8cefb47cd85b
Sami Haroon <abdulsami.se@gmail.com>
2022-08-22 03:50:49 +0500

    Update 78.subsets.js

--- 9-Recursion/78.subsets.js
+++ 9-Recursion/78.subsets.js
@@ -8,9 +8,28 @@
 /**
  * @param {number[]} nums
  * @return {number[][]}
+ * @runtime O(N*N)
+ * @runtime O(N*N)
  */
-var subsets = function(nums) {
+ var subsets = function(nums) {
     
+    const powerset = [];
+    
+	generatePowerset([], 0);
+    
+    /** Recursive Function **/
+	function generatePowerset(path, index) {
+        
+        // console.log(path, index, powerset);
+        
+		powerset.push(path);
+		for (let i = index; i < nums.length; i++) {
+            // pushing previous path and current number as an array, and moving to next index
+			generatePowerset([...path, nums[i]], i + 1);
+		}
+	}
+
+	return powerset;
 };
 // @lc code=end
 
