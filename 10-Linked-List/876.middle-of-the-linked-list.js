28e4f492b55b0fddcbd12f793c96f6c43a5e1997
Sami Haroon <abdulsami.se@gmail.com>
2022-08-23 02:04:49 +0500

    Update 876.middle-of-the-linked-list.js

--- 10-Linked-List/876.middle-of-the-linked-list.js
+++ 10-Linked-List/876.middle-of-the-linked-list.js
@@ -13,11 +13,37 @@
  * }
  */
 /**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
  * @param {ListNode} head
  * @return {ListNode}
+ * @runtime O(n)
+ * @space O(1)
  */
-var middleNode = function(head) {
+ var middleNode = function(head) {
     
+    if(head == null || head.next == null) return head;
+    
+    let fast = slow = head;
+    while (fast && fast.next) {
+        
+        fast = fast.next.next;
+        slow = slow.next;
+        
+    }
+    return slow;
 };
+
+/**
+
+[1,2,3,4,5]
+[1,2,3,4,5,6]
+
+**/
 // @lc code=end
 
