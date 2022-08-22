b2ad7ebbe854666d5061b953d7da1fcb7adcb8ec
Sami Haroon <abdulsami.se@gmail.com>
2022-08-23 01:51:22 +0500

    Update 141.linked-list-cycle.js

--- 10-Linked-List/141.linked-list-cycle.js
+++ 10-Linked-List/141.linked-list-cycle.js
@@ -14,11 +14,55 @@
  */
 
 /**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
  * @param {ListNode} head
  * @return {boolean}
+ * @runtime O(n)
+ * @space O(1)
  */
-var hasCycle = function(head) {
+ var hasCycle = function(head) {
     
+    if(head == null || head.next == null) return false;
+    
+    let slow = head;
+    let fast = head.next;
+    
+    while(fast && fast.next){
+        // fast moves by two
+        fast = fast.next.next
+        // slow moves by one
+        slow = slow.next
+        // two pointers meet, cycle
+        if(fast === slow){
+            return true
+        }
+    }
+    
+    return false;  
 };
+
+/**
+
+[]
+-1
+[1,1,1,1]
+-1
+[1,2]
+-1
+[3,2,0,-4]
+1
+[1,2]
+0
+[1]
+-1
+
+**/
 // @lc code=end
 
