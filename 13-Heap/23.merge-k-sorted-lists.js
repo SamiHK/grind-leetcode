8c557713fde9949e4c483030f7227d3889bc37bf
Sami Haroon <abdulsami.se@gmail.com>
2022-08-23 19:59:55 +0500

    Update 13-Heap/23.merge-k-sorted-lists.js

--- 13-Heap/23.merge-k-sorted-lists.js
+++ 13-Heap/23.merge-k-sorted-lists.js
@@ -13,10 +13,65 @@
  * }
  */
 /**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
  * @param {ListNode[]} lists
  * @return {ListNode}
+ * @runtime O(n * m)
+ * @space O(m)
  */
-var mergeKLists = function(lists) {
+ var mergeKLists = function(lists) {
+    
+    if(lists === null || lists.length === 0) return null;
+        
+    for(let i = 0; i < (lists.length -1); i++){ // if there are three elements, then run from 0 to 1 only
+        
+        // recursive merge
+        lists[i+1] = mergeLists(lists[i], lists[i+1]);
+        
+    }
+    
+    return lists[lists.length-1];
+};
+
+
+
+const mergeLists = (left, right) => {
+    
+    let result = new ListNode();
+    let tail = result;
+    
+    
+    while(left !== null && right !== null) {
+        if(left.val < right.val){
+            tail.next =left;
+            left = left.next; // move left ahead
+        } else {
+            tail.next = right;
+            right = right.next; // move right ahead
+        }
+        
+        tail = tail.next //move tail ahead
+    }
+    
+    // if left have some nodes remaining
+    if(left){
+        tail.next = left;
+    }
+    
+    // if right have some nodes remaining
+    if(right){
+        tail.next = right;
+    }
+    
+    
+    // return the frst actual node attached to result.
+    return result.next;
     
 };
 // @lc code=end
--- 13-Heap/973.k-closest-points-to-origin.js
+++ 13-Heap/973.k-closest-points-to-origin.js
@@ -5,13 +5,56 @@
  */
 
 // @lc code=start
-/**
- * @param {number[][]} points
- * @param {number} k
- * @return {number[][]}
- */
 var kClosest = function(points, k) {
+    if(points.length <= 0) return [];
+    if(k === 0) return [];
+    if(k > points.length) return [];
+    if(k === points.length) return points;
     
+    for(let i = 0; i < points.length; i++){
+        let point = points[i];
+        points[i].push((point[0]*point[0] + point[1]*point[1]));
+    }
+    
+    points.sort(([x1, y1, z1], [x2, y2, z2]) => z1 - z2); // ascending order of distance
+    return points.slice(0, k).map(([x, y, z]) => [x, y]);
 };
+
+
+// var kClosest = function(points, k) {
+  
+    
+//     if(points.length <= 0) return [];
+//     if(k === 0) return [];
+//     if(k > points.length) return [];
+//     if(k === points.length) return points;
+//     // if(points.length == 2 && k == 1) return ((points[0][0]**2 + points[0][1]**2) > (points[1][0]**2 + points[1][1]**2)) ? [points[1]]:[points[0]]
+    
+//     for(let i = 0; i < points.length; i++){
+//         let point = points[i];
+//         points[i].push((point[0]*point[0] + point[1]*point[1]));
+//     }
+    
+//     let bucket = new Array();
+    
+//     for(let i = 0; i < points.length; i++){
+        
+//         (bucket[points[i][2]]) ?
+//             bucket[points[i][2]].push([points[i][0], points[i][1]]) 
+//                 : bucket[points[i][2]] = [[points[i][0], points[i][1]]];
+//     }
+    
+//     // now iterating over bucket will give us the results
+//     let res = [];
+//     for(let i = 0; i < bucket.length; i++){
+//         if(bucket[i]){
+//             res = [...res, ...bucket[i]];
+//         }
+//         if(res.length - k >= 0){
+//             return res;
+//         }
+//     }
+//     return res;
+// };
 // @lc code=end
 
