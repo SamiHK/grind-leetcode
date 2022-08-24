7108916a117359ee82a0c3a523a943113bee37f1
Sami Haroon <abdulsami.se@gmail.com>
2022-08-24 17:09:49 +0500

    Update 53.maximum-subarray.js

--- 15-Dynamic-Programming/53.maximum-subarray.js
+++ 15-Dynamic-Programming/53.maximum-subarray.js
@@ -8,9 +8,27 @@
 /**
  * @param {number[]} nums
  * @return {number}
+ * T: O(n), S: O(1)
  */
-var maxSubArray = function(nums) {
+ var maxSubArray = function(nums) {
     
+    if(nums.length === 0) return [];
+    if(nums.length === 1) return nums[0];
+    
+    let currSum = 0;
+    let maxSum = nums[0];
+    
+    for(num of nums){
+        
+        // if current sum is -ve then adding this will not make sum larger
+        if(currSum < 0) currSum = 0;
+        
+        currSum = currSum + num;
+        if(currSum > maxSum) maxSum = currSum;
+    }
+    
+    
+    return maxSum;
 };
 // @lc code=end
 
