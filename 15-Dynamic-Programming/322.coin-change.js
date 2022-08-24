39d9fd101f7204a41e9a35ccdca75a3e9e9b9481
Sami Haroon <abdulsami.se@gmail.com>
2022-08-24 17:59:26 +0500

    Update 322.coin-change.js

--- 15-Dynamic-Programming/322.coin-change.js
+++ 15-Dynamic-Programming/322.coin-change.js
@@ -9,9 +9,47 @@
  * @param {number[]} coins
  * @param {number} amount
  * @return {number}
+ * BRUTE FORCE
  */
-var coinChange = function(coins, amount) {
+ var coinChange = function(coins, amount, memo = {}) {
     
+    if(amount in memo) return memo[amount];
+    if(amount === 0) return 0;
+    if(amount < 0) return -1;
+    
+    let shortestCombination = -1;
+    
+    for(let coin of coins){
+        
+        let remainder = amount - coin;
+        let remainderCombination = coinChange(coins, remainder, memo);
+        
+        if(remainderCombination !== -1){ // target found
+            const combination = remainderCombination+1;
+            
+            if(shortestCombination === -1){
+                shortestCombination = combination;
+                continue;
+            }
+            
+            if(shortestCombination > combination){
+                shortestCombination = combination
+            }
+        }
+    }
+    memo[amount] = shortestCombination;
+    return shortestCombination;
 };
+
+// remove from target each node and see remainder
+//             11
+//         /   |  \
+//        10   9    6
+//                 / | \
+//                5  4  1
+//                 \    /
+//                  0   0                          
+    
+// shortest is [5,1,5] OR [5,5,1]
 // @lc code=end
 
