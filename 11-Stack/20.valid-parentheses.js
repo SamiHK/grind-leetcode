a6fa5b285a0ffd953d3bc49498b6f3c1610ff6df
Sami Haroon <abdulsami.se@gmail.com>
2022-08-24 13:36:44 +0500

    Update 20.valid-parentheses.js

--- 11-Stack/20.valid-parentheses.js
+++ 11-Stack/20.valid-parentheses.js
@@ -9,8 +9,36 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function(s) {
     
+    let closeToOpen = {
+        ')':'(',
+        ']':'[',
+        '}':'{'
+    }
+    
+    if( s == null || s == [] || (s.length ==0 && s[0] in closeToOpen)) return false;
+     
+    let stack = [];
+    for(let i = 0; i < s.length; i++){
+        
+        if(s[i] === '(' || s[i] === '{' || s[i] === '['){
+            stack.push(s[i]);
+        } else{
+            if(stack == []) false;
+            if(s[i] in closeToOpen && stack[stack.length-1] == closeToOpen[s[i]]){
+                stack.pop();
+            } else {
+                return false
+            }
+        }
+    }
+    
+    return stack.length === 0 ? true: false;
 };
 // @lc code=end
 
