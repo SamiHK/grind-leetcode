dee5bceb98fc105a4b170332b6bd2dcb9b786041
Sami Haroon <abdulsami.se@gmail.com>
2022-08-24 15:22:05 +0500

    Update 42.trapping-rain-water.js

--- 11-Stack/42.trapping-rain-water.js
+++ 11-Stack/42.trapping-rain-water.js
@@ -8,9 +8,36 @@
 /**
  * @param {number[]} height
  * @return {number}
+ * T: O(n)
+ * S: O(1)
  */
-var trap = function(height) {
+ var trap = function(height) {
     
+    let left = 0;
+    let right = height.length-1;
+    let leftMax = 0
+    let rightMax = 0;
+    let result = 0;
+    
+    
+    while(left <= right){
+        let diff = 0;
+        leftMax = Math.max(leftMax, height[left]);
+        rightMax = Math.max(rightMax, height[right]);
+        
+        if(height[left] < height[right]){
+            diff = leftMax - height[left];
+            left++
+        } else {
+            diff = rightMax - height[right];
+            right--
+        }
+        
+        result += diff;
+    }
+    
+    return result;
 };
+
 // @lc code=end
 
