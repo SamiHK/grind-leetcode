c4f9c81ab8ce0818ebd8060ee75f538843f1d799
Sami Haroon <abdulsami.se@gmail.com>
2022-08-26 04:51:34 +0500

    Update 235.lowest-common-ancestor-of-a-binary-search-tree.js

--- 6-Binary-Search-Tree/235.lowest-common-ancestor-of-a-binary-search-tree.js
+++ 6-Binary-Search-Tree/235.lowest-common-ancestor-of-a-binary-search-tree.js
@@ -14,13 +14,36 @@
  */
 
 /**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
  * @param {TreeNode} root
  * @param {TreeNode} p
  * @param {TreeNode} q
  * @return {TreeNode}
+ * @runtime O(n)
+ * @space O(1)
  */
-var lowestCommonAncestor = function(root, p, q) {
+ var lowestCommonAncestor = function(root, p, q) {
+    if (root == null) { return null; }
+    // Ancestor is Root
+    // In BST no two nodes can have a common ancestor if root lies in between them else traverse root
     
+    // if root is bigger than both left and right nodes, shift root to left side(smaller)
+    if (root.val > p.val && root.val > q.val) {
+      return lowestCommonAncestor(root.left, p, q);
+    // if root is lesser than both left and right nodes, shift root to right side(bigger)
+    } else if (root.val < p.val && root.val < q.val) {
+      return lowestCommonAncestor(root.right, p, q);
+    } else {
+    // if p < root < q || p == root || q == root 
+      return root;
+    }
 };
 // @lc code=end
 
