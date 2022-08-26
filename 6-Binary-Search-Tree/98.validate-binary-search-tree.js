bf564d09f74293ed0d6b5c6c87908ddc32daa48f
Sami Haroon <abdulsami.se@gmail.com>
2022-08-26 05:42:34 +0500

    Update 98.validate-binary-search-tree.js

--- 6-Binary-Search-Tree/98.validate-binary-search-tree.js
+++ 6-Binary-Search-Tree/98.validate-binary-search-tree.js
@@ -14,35 +14,30 @@
  * }
  */
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
  * @param {TreeNode} root
  * @return {boolean}
  * @runtime O(n)
  * @space O(1)
  */
  var isValidBST = function(root) {
-    return findValidBST(root, -Infinity, Infinity)    
+    
+    const ascSortArr = inOrder(root);
+    
+    for(let i = 0; i < ascSortArr.length; i++) {
+        if(ascSortArr[i] >= ascSortArr[i+1]) return false;
+    }
+    
+    return true;
 };
 
-var findValidBST = function(root, minVal, maxVal) {
-    
-    if(root === null) return true;
-    if (root.val >= maxVal || root.val <= minVal) return false;
-    
-    // if goes left, root becomes right
-    let isLeftValid = findValidBST(root.left, minVal, root.value)
-    
-    // if goes right, root becomes left
-    let isRightValid = findValidBST(root.right, root.val, maxVal)
-    
-    return isLeftValid && isRightValid;
+
+var inOrder = (node) => {
+    if(node === null) return [];
+    return [...inOrder(node.left), node.val, ...inOrder(node.right)]
 }
+
+// IN-ORDER Traverse gives sorted array for BST
+// PRE-ORDER Traverse gives Max-Heap if there are no empty leaves
+// POST-ORDER Traverse gives Min-Heap if there are no empty leaves
 // @lc code=end
 
