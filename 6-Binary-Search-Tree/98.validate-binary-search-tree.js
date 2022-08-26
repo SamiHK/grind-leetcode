813a0d392d270c13389e07b63e72b9d141cd90af
Sami Haroon <abdulsami.se@gmail.com>
2022-08-26 05:23:40 +0500

    Update 98.validate-binary-search-tree.js

--- 6-Binary-Search-Tree/98.validate-binary-search-tree.js
+++ 6-Binary-Search-Tree/98.validate-binary-search-tree.js
@@ -14,11 +14,35 @@
  * }
  */
 /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
  * @param {TreeNode} root
  * @return {boolean}
+ * @runtime O(n)
+ * @space O(1)
  */
-var isValidBST = function(root) {
-    
+ var isValidBST = function(root) {
+    return findValidBST(root, -Infinity, Infinity)    
 };
+
+var findValidBST = function(root, minVal, maxVal) {
+    
+    if(root === null) return true;
+    if (root.val >= maxVal || root.val <= minVal) return false;
+    
+    // if goes left, root becomes right
+    let isLeftValid = findValidBST(root.left, minVal, root.value)
+    
+    // if goes right, root becomes left
+    let isRightValid = findValidBST(root.right, root.val, maxVal)
+    
+    return isLeftValid && isRightValid;
+}
 // @lc code=end
 
